984c31eac2b63e85f83564b2bf7e6977f206f859
catcherindarah <22rray@athenian.org>
2021-03-04 06:02:01 +0000

    finished

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,16 +4,55 @@ public class MyMain {
 
     // Sorts the ArrayList using insertion sort
     public static ArrayList<Integer> insertionSort(ArrayList<Integer> list) {
-        // YOUR CODE HERE
-        return null;
+        for(int i = 0; i <list.size(); i++)
+        {
+            int a = list.get(i);
+            int k = i - 1;
+            while(k > -1 && list.get(a) > k)
+            {
+                list.set(k+1, list.get(k));
+                k--;
+            }
+            list.set(k+1, a);
+        }
+        return list;
     }
 
      // Merges two sorted arrays into one large combined
      // sorted array
      // You may assume arr1 and arr2 are the same length
     public static int[] merge(int[] arr1, int[] arr2) { 
-        // YOUR CODE HERE
-        return null;
+        int [] rah = new int[arr1.length + arr2.length];
+        int i = 0;
+        int k = 0;
+        int j = 0;
+        while(i < arr1.length && k < arr2.length)
+        {
+            if(arr1[i] < arr2[k])
+            {
+                rah[j] = arr1[i];
+                i++;
+            }
+            else
+            {
+                rah[j] = arr2[k];
+                k++;
+            }
+            j++;
+        }
+        while(i < arr1.length)
+        {
+            rah[j] = arr1[i];
+            i++;
+            j++;
+        }
+        while(k < arr2.length)
+        {
+            rah[j] = arr2[k];
+            i++;
+            k++;
+        }
+        return rah;
     }
 
     public static void main(String[] args) {
